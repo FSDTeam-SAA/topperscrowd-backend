aa29fd9f0e8282b8bb82df307cb1b1799284d03f
Nsarkar-XLR8 <knayemlol24@gmail.com>
2026-06-02 18:09:57 +0600

    chore: add whitespace padding to express configuration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,9 @@ import { apiReference } from "@scalar/express-api-reference";
 const app: Application = express();
 app.set("trust proxy", 1);
 
+
+
+
 app.use(express.static("public"));
 if (config.NODE_ENV === "development") {
   app.use(morgan("dev"));
